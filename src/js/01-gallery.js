c76809d44ef3fb0c868fd4247e3d86e067d4b30a
NataliaPavlovskaya <91052774+NataliaPavlovskaya@users.noreply.github.com>
2022-04-28 15:44:14 +0300

    com

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,51 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+const galleryEl = document.querySelector('.gallery');
+
+// 1.Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+
+function createImageGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+    </div>`;
+    })
+    .join('');
+}
+galleryEl.insertAdjacentHTML('beforeend', createImageGallery(galleryItems));
+
+
+
+// galleryEl.addEventListener('click', onOpenLargeImg);
+// function onOpenLargeImg(event) {
+//     event.preventDefault();
+
+//     const isImagesEl = event.target.classList.contains('gallery__image');
+//   if (!isImagesEl) {
+//     return;
+//   }
+  
+//     const largeImgLink = event.target.dataset.source;
+
+
+//     const instance = basicLightbox.create(`
+//   <img 
+//   src="${largeImgLink}" 
+//   alt="${event.target.alt}">
+//   `);
+//   instance.show();
+// };
+
 
 console.log(galleryItems);
+
+
