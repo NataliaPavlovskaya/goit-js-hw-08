1b9cc24d4c20ab29887934c64ced40106c4524a4
NataliaPavlovskaya <91052774+NataliaPavlovskaya@users.noreply.github.com>
2022-05-01 20:46:23 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,22 @@
+import throttle from 'lodash.throttle';
+import Player from "@vimeo/player";
 
-// рпар
+const player = new Vimeo.Player(iframe);
+
+const LOCALSTORAGE_KEY = "videoplayer-current-time"
+
+player.on('timeUpdate', throttle(data => localStorage.setItem(LOCALSTORAGE_KEY, data.seconds), 1000),);
+
+player.on('loaded', updateTime); 
+
+function updateTime(){
+    const savedTime = localStorage.getItem(LOCALSTORAGE_KEY);
+    player.setCurrentTime(savedTime).then(function(seconds){
+        console.log(seconds);
+    }).catch(function(error){
+        switch(error.name){
+            case 'RangeError':
+                break;      
+        }
+    })
+};
