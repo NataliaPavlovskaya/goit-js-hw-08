40594539dddfc0c442867b59a952468eeb4bc1c5
NataliaPavlovskaya <91052774+NataliaPavlovskaya@users.noreply.github.com>
2022-05-01 22:02:20 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,42 @@
 import throttle from 'lodash.throttle';
 import Player from "@vimeo/player";
 
+const iframe = document.querySelector('iframe'); 
+
+
 const player = new Vimeo.Player(iframe);
 
 const LOCALSTORAGE_KEY = "videoplayer-current-time"
-
-player.on('timeUpdate', throttle(data => localStorage.setItem(LOCALSTORAGE_KEY, data.seconds), 1000),);
-
-player.on('loaded', updateTime); 
-
-function updateTime(){
-    const savedTime = localStorage.getItem(LOCALSTORAGE_KEY);
-    player.setCurrentTime(savedTime).then(function(seconds){
-        console.log(seconds);
-    }).catch(function(error){
-        switch(error.name){
-            case 'RangeError':
-                break;      
-        }
-    })
+   
+player.on('timeupdate', throttle(data => localStorage.setItem(LOCALSTORAGE_KEY, data.seconds), 1000),);
+const savedTime = localStorage.getItem(LOCALSTORAGE_KEY)
+if(savedTime){
+    player.setCurrentTime(savedTime);
 };
+
+
+
+
+
+
+
+
+
+// function updateTime(){
+//     ;
+//     player.setCurrentTime(savedTime).then(function(seconds){
+//         console.log(seconds);
+//     }).catch(function(error){
+//         switch(error.name){
+//             case 'RangeError':
+//                 break;      
+//         }
+//     })
+// };
+
+
+// function onPlay(){
+//     player.getCurrentTime().then(function(seconds) {
+//     localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+//         });
+// };  
